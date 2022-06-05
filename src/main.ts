7fe837e17bc3f136de4002bbce4872e9198dbee5
Ristic Milan <risticmilan89@gmail.com>
2022-06-05 09:02:45 +0200

    generated resources

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`server started on port ${port}`);
 }
+
 bootstrap();
